a9214426c76faf1a552105f817b2f3edb4dec9af
UrielEscazan <escazan2.0@gmail.com>
2019-09-20 20:28:20 -0500

    little cambio

--- src/components/cards.js
+++ src/components/cards.js
@@ -8,15 +8,15 @@ function Tarjeta(props) {
         <img src="https://www.spirit-of-metal.com/les%20goupes/T/The%20Devil%20Wears%20Prada/pics/9a11_3.jpg" className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">Biografia</h5>
-          <p className="card-text">The Devil Wears Prada (abreviado como TDWP) es una banda de metalcore de Dayton, Estados Unidos. Están firmados actualmente en Rise Records y son clasificados en PureVolume y Myspace como una banda de metalcore cristiano. Tomaron el nombre de la novela y película. La banda fue formada antes de salir la película del mismo nombre. Aunque la inspiración de la banda para su nombre era del libro, pero no el motivo. Según lo dicho por el vocalista Mike Hranica en el blog de la bandaa.</p>
+          <p className="card-text">The Devil Wears Prada (abreviado como TDWP) es una banda de metalcore de Dayton, Estados Unidos. Están firmados actualmente en Rise Records y son clasificados en PureVolume y Myspace como una banda de metalcore cristiano. Tomaron el nombre de la novela y película. La banda fue formada antes de salir la película del mismo nombre. Aunque la inspiración de la banda para su nombre era del libro, pero no el motivo. Según lo dicho por el vocalista Mike Hranica en el blog de la banda.</p>
           <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
         </div>
         <button type="button" class="btn btn-danger" href="https://www.youtube.com/watch?v=HfiaUIYF4ds">Ver video de la banda</button>
       </div>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src="https://ep01.epimg.net/cultura/imagenes/2018/04/16/actualidad/1523892028_047036_1524501429_noticia_normal.jpg" className="card-img-top" alt="La pegatina"/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Biografia</h5>
           <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
           <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
         </div>
